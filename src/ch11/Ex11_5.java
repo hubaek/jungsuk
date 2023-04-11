1e28eef483066382d959d9cc54e8d749bc6be24c
hubaek <zjfl3122@naver.com>
2023-04-11 23:29:38 +0900

    commit test2

--- src/ch11/Ex11_5.java
+++ src/ch11/Ex11_5.java
@@ -47,7 +47,7 @@ public class Ex11_5 {
         list.add(new Student("남궁성",1,2,90,70,80));
         list.add(new Student("김자바",1,3,80,80,90));
         list.add(new Student("이자바",1,4,70,90,70));
-        list.add(new Student("안자바",1,5,60,100,80));
+        list.add(new Student("안자바",1,5,70,100,80));
 
         Collections.sort(list);
         Iterator it = list.iterator();
